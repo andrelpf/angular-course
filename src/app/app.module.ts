70b94aea1919d26bb51b559b8b1462e9c4b8eb26
André Freitas <andre.or.luuiz@gmail.com>
2021-05-25 15:41:50 -0300

    First Assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { MatButtonModule, MatCardModule, MatGridListModule, MatListModule, MatTo
 import { FlexLayoutModule } from '@angular/flex-layout';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
